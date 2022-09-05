a9e1836c63c1bdf14ea3188f3bfcdf0f50697350
Elektron97 <danele.caradonna@gmail.com>
2022-09-05 17:14:22 +0200

    RSoft Renda

---
+++ MATLAB/Renda_Model/RSoft_InvertedPendulum/simulation.m
@@ -0,0 +1,16 @@
+%%%%%%%%%%%% Simulation and Analysis %%%%%%%%%
+close all
+clear all
+clc
+
+%% Load R-SIP Structure
+load("R_sip.mat");
+
+%% Plot Initial Configuration
+% T1.plotq0
+
+%% Statics Solution
+% T1.statics;
+
+%% Dynamics Solution
+T1.dynamics;
