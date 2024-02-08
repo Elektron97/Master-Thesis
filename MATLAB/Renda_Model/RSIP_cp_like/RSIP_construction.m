aaf6ee377e7a4ba20cc6237dc1d40ed8bde39bb3
Daniele <daniele.caradonna@hotmail.com>
2024-02-08 23:51:12 +0100

    new segment with cp parameters

--- MATLAB/Renda_Model/RSIP_cp_like/RSIP_construction.m
+++ MATLAB/Renda_Model/RSIP_cp_like/RSIP_construction.m
@@ -9,14 +9,23 @@ my_startup
 cd RSIP_cp_like
 
 %% Parameters
-% length = 1.0;       % Length of SIP
-% radius = 0.1;      % Radius of SIP
-% k_rev = 0;       % Joint Stiffness
-% rho = 1070;         % Density
-% E = 0.061424e6;     % Young Modulus
-% v = 0.5;            % Poissons Ratio
-% damp = 0.112e5;     % Damping
-% dampR = 0.02;       % Damping of Rotoidal joint
+length = 1.0;                   % Length of SIP
+radius = 0.05;                  % Radius of SIP
+k_rev = 0;                      % Joint Stiffness
+rho = 1/(pi*length*radius^2);    % Density
+J_z = (pi/4)*(radius)^4;
+E = 1.0/J_z;                    % Young Modulus
+v = 0.5;                        % Poissons Ratio
+damp = 0.1/(3*J_z);             % Damping
+dampR = 0.5;                    % Damping of Rotoidal joint
+
+%% Display the parameters
+disp("Length: " + num2str(length));
+disp("Radius: " + num2str(radius));
+disp("rho: " + num2str(rho));
+disp("Young Modulus: " + num2str(E));
+disp("Damping: " + num2str(damp));
+
 %% Links Definition
 % L1 = SorosimLink;
 % save("Link_Properties2.mat");
@@ -24,10 +33,10 @@ load("Link_Properties2.mat");
 
 %% Open Chain Definition
 T1 = SorosimLinkage(L1);
-save("R_sip2.mat");
 % load("R_sip2.mat")
 %% Plot Initial Configuration
-T1.plotq0
+% T1.plotq0
 
 %% Save R-SIP
-% save("R_sip2.mat", 'T1')
+save("R_sip2.mat", 'T1')
+save("R_sip_cp.mat", 'T1')
